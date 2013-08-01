931cce5c3016a9f575fc1bf7e46a59fa4e1ce7a6
lalopmak <lalop.llmao@gmail.com>
2013-08-01 01:41:46 -0500

    Added more "dot" fancy characters and reorganized the list

--- Text.py
+++ Text.py
@@ -9,7 +9,21 @@ from itertools import *
 from PyQt4.QtCore import *
 
 #some undesired fancy characters to replace
-fancy2normal = [(u"“",u'"'),(u"”",u'"'),(u"’",u"'"),(u"…",u"...")]
+fancy2normal = [
+    #transformations to dots
+    (u"…",u"..."),(u". . .",u"..."),
+
+    #trimming of dots; put after transformations
+    (u" ... ",u"..."),        
+    
+    #dashes
+    (u"—",u"-"),
+    
+    #quotes
+    (u"“",u'"'),
+    (u"”",u'"'),
+    (u"’",u"'")
+]
 
 class SentenceSplitter(object):
 
