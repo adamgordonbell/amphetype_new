51ce59d5152db8bc832c10bc6ef6932eab7cc28e
Adam Bell <agbell@gmail.com>
2014-02-04 08:49:33 -0500

    improved symbol drilling

--- Quizzer.py
+++ Quizzer.py
@@ -411,10 +411,11 @@ class Quizzer(QWidget):
     def modifiedWord(self, word):
         global wordCache
         if not word in wordCache:
+            symbols = random.choice(Settings.get('include_symbols').split(" "));
             if (not any((c in Settings.get('stop_symbols')) for c in word)) and (len(word) > 1) and (Settings.get('title_case')) and (Settings.get('symbols')):
-                wordCache[word] = word[0].capitalize() + word[1:] + random.choice(Settings.get('include_symbols'))
+                wordCache[word] = symbols.replace("0",(word[0].capitalize() + word[1:]))
             elif(not any((c in Settings.get('stop_symbols')) for c in word)) and (len(word) > 1) and (Settings.get('symbols')):
-                wordCache[word] = word + random.choice(Settings.get('include_symbols'))
+                wordCache[word] = symbols.replace("0",word )
             elif(not any((c in Settings.get('stop_symbols')) for c in word)) and (len(word) > 1) and (Settings.get('title_case')):
                 wordCache[word] = word[0].capitalize() + word[1:]
             else:
